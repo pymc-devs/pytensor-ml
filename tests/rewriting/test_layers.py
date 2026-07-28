118ca99d46c16fc12aa878b46c7beee9a5c3bd58
jessegrabowski <jessegrabowski@gmail.com>
2026-07-28 17:51:26 -0500

    Assert rewritten graphs instead of counting ops in rewrite tests

--- tests/rewriting/test_layers.py
+++ tests/rewriting/test_layers.py
@@ -1,68 +1,56 @@
 import pytensor.tensor as pt
 import pytest
 
-from pytensor.graph import graph_inputs
-from pytensor.graph.fg import FunctionGraph
+from pytensor.graph.basic import equal_computations
 
-from pytensor_ml.layers import (
-    BatchNorm2D,
-    BatchNormLayer,
-    Dropout,
-    DropoutLayer,
-    Linear,
-    PredictionBatchNormLayer,
-    Sequential,
-)
+from pytensor_ml.layers import BatchNorm2D, Dropout, Linear, PredictionBatchNormLayer, Sequential
 from pytensor_ml.pytensorf import rewrite_for_prediction
 
 
-@pytest.fixture()
-def feature_extractor_and_rng():
-    d1 = Dropout("Dropout_1", p=0.5)
-    d2 = Dropout("Dropout_2", p=0.5)
-    feature_extractor = Sequential(
-        Linear("Layer_1", n_in=6, n_out=3), d1, Linear("Layer_2", n_in=3, n_out=1), d2
-    )
-
-    rngs = [d1.rng, d2.rng]
-
-    return feature_extractor, rngs
-
-
-def test_remove_dropout(feature_extractor_and_rng):
-    feature_extractor, rngs = feature_extractor_and_rng
-
+def test_remove_dropout():
+    first_layer = Linear("Layer_1", n_in=6, n_out=3)
+    second_layer = Linear("Layer_2", n_in=3, n_out=1)
     X = pt.tensor("X", shape=(None, 6))
-    latent = feature_extractor(X)
 
-    fg = FunctionGraph(inputs=list(graph_inputs([latent])) + rngs, outputs=[latent])
+    with_dropout = Sequential(
+        first_layer, Dropout("Dropout_1", p=0.5), second_layer, Dropout("Dropout_2", p=0.5)
+    )(X)
+    without_dropout = Sequential(first_layer, second_layer)(X)
 
-    assert len([node.op for node in fg.apply_nodes if isinstance(node.op, DropoutLayer)]) == 2
-    fg = rewrite_for_prediction(fg)
-
-    assert len([node.op for node in fg.apply_nodes if isinstance(node.op, DropoutLayer)]) == 0
+    assert equal_computations([rewrite_for_prediction(with_dropout)], [without_dropout])
 
 
 @pytest.mark.parametrize(
     "consumed_downstream", [False, True], ids=["as_graph_output", "consumed_by_a_layer"]
 )
 def test_rewrite_batch_stats_to_running_average_stats(consumed_downstream):
-    layers = [Linear("Layer_1", n_in=6, n_out=3), BatchNorm2D()]
-    if consumed_downstream:
-        # A downstream SymbolicOp type-checks its inputs more strictly than a graph output does.
-        layers.append(Linear("Layer_2", n_in=3, n_out=1))
-
-    feature_extractor = Sequential(*layers)
+    linear = Linear("Layer_1", n_in=6, n_out=3)
+    batch_norm = BatchNorm2D("bn", n_in=3)
+    # A downstream SymbolicOp type-checks its inputs more strictly than a graph output does.
+    head = Linear("Layer_2", n_in=3, n_out=1) if consumed_downstream else None
     X = pt.tensor("X", shape=(None, 6))
-    latent = feature_extractor(X)
 
-    fg = FunctionGraph(inputs=list(graph_inputs([latent])), outputs=[latent])
-    assert len([node.op for node in fg.apply_nodes if isinstance(node.op, BatchNormLayer)]) == 1
+    normalized = batch_norm(linear(X))
+    trained = head(normalized) if head else normalized
 
-    fg = rewrite_for_prediction(fg)
-
-    assert not any(isinstance(node.op, BatchNormLayer) for node in fg.apply_nodes)
-    assert (
-        len([node.op for node in fg.apply_nodes if isinstance(node.op, PredictionBatchNormLayer)])
-        == 1
+    prediction_normalized = PredictionBatchNormLayer(
+        name="bn", n_in=3, epsilon=batch_norm.epsilon, affine=True
+    )(
+        linear(X),
+        batch_norm.loc,
+        batch_norm.scale,
+        batch_norm.running_mean,
+        batch_norm.running_var,
     )
+    expected = head(prediction_normalized) if head else prediction_normalized
+
+    assert equal_computations([rewrite_for_prediction(trained)], [expected])
+
+
+def test_rewrite_leaves_batch_norm_without_running_stats_alone():
+    X = pt.tensor("X", shape=(None, 6))
+    normalized = Sequential(
+        Linear("Layer_1", n_in=6, n_out=3), BatchNorm2D("bn", track_running_stats=False)
+    )(X)
+
+    assert equal_computations([rewrite_for_prediction(normalized)], [normalized])
