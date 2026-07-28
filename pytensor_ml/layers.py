aeba7de5767e47db71f0ddca93b1adaeb2945e46
jessegrabowski <jessegrabowski@gmail.com>
2026-07-28 11:50:09 -0500

    Sort the ops import in layers.py

--- pytensor_ml/layers.py
+++ pytensor_ml/layers.py
@@ -9,13 +9,13 @@ import pytensor.tensor.random as ptr
 from pytensor import config
 from pytensor.compile.sharedvalue import shared
 
+from pytensor_ml.ops import LayerOp, UnaryLayerOp
 from pytensor_ml.params import (
     NonTrainableParameter,
     TrainableParameter,
     non_trainable,
     trainable,
 )
-from pytensor_ml.ops import LayerOp, UnaryLayerOp
 
 
 def shape_to_str(shape):
